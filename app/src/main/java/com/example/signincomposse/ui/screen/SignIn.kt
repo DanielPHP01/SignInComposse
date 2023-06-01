7bb4818512d836870e8a62f413c64d2fe7d699b4
Dom <DANIELPHP01>
2023-06-02 00:12:36 +0600

    update

--- app/src/main/java/com/example/signincomposse/ui/screen/SignIn.kt
+++ app/src/main/java/com/example/signincomposse/ui/screen/SignIn.kt
@@ -7,8 +7,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -17,17 +18,24 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.signincomposse.R
+import com.example.signincomposse.ui.theme.SignInColor
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun SignInScreen() {
+    var text by remember {
+        mutableStateOf("")
+    }
+
     Column(modifier = Modifier
         .fillMaxSize()
         .background(Color.White)
     ) {
         Row(modifier = Modifier
             .fillMaxWidth()
-            .background(Color.White).padding(top = 40.dp)) {
+            .background(Color.White)
+            .padding(top = 40.dp)) {
             Text(
                 text = "Hello,\n" +
                         "Welcome!",
@@ -37,6 +45,21 @@ fun SignInScreen() {
 
             )
             Image(painter = painterResource(id = R.drawable.illustration_2), contentDescription = "" )
+        }
+        TextField(value = text, onValueChange = {text = it}, colors =TextFieldDefaults.textFieldColors(placeholderColor = Color.White), modifier = Modifier.background(
+            Color.White)
+        )
+        Button(
+            onClick = { },
+            shape = RoundedCornerShape(15.dp),
+            colors = ButtonDefaults.buttonColors(SignInColor),
+            modifier = Modifier
+                .padding(top = 120.dp)
+        ) {
+            Text(
+                text = "Sign in",
+                fontSize = 40.sp,
+            )
         }
     }
 }
